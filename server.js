e0e02002c2f7434734a591c8d6af3fc8c331f1ae
Trent Liu <trentliu92@gmail.com>
2018-06-15 01:32:34 -0700

    init pi water server code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const port = 3000;
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`App started on port ` + port));
+
+app.get('/', function(req, res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send("pi-server connected");
+});
+
+ app.get('/waterplants', function(req, res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send({"data": "watering"});
+});
+
+
